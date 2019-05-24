f84259de15aa76e26245da1f14ccc11c3de35d7e
League Student <league@iMac.attlocal.net>
2019-05-24 16:54:01 -0700

    Today, I worked on typing-tutor (Couldn't figure out how to update the JLable)

--- src/_05_typing_tutor/typingtutor.java
+++ src/_05_typing_tutor/typingtutor.java
@@ -1,10 +1,79 @@
 package _05_typing_tutor;
 
-public class typingtutor {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Random;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class typingtutor implements KeyListener {
+
+    JFrame jframe= new JFrame();
+    
+   
+	char currentLetter=generateRandomLetter();
+	JLabel jlabel= new JLabel(""+currentLetter);
+	JPanel jpanel= new JPanel();
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		typingtutor tutor= new typingtutor();
+	     tutor.run();
 
+
+	}
+	public void run() {
+		
+		
+		
+	
+	
+		
+		jlabel.setFont(jlabel.getFont().deriveFont(28.0f));
+		jlabel.setHorizontalAlignment(JLabel.CENTER);
+		
+		jpanel.add(jlabel);
+		
+		jframe.add(jpanel);
+	    jframe.setVisible(true);
+	    
+	    jframe.addKeyListener(this);
+        jframe.setSize(800, 800);
+		
+	}
+    char generateRandomLetter() {
+        Random r = new Random();
+        return (char) (r.nextInt(26) + 'a');
+  }
+	@Override
+	public void keyTyped(KeyEvent e) {	
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		System.out.println("You typed "+ e.getKeyChar());
+		
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		typingtutor type= new typingtutor();
+		  currentLetter= generateRandomLetter();
+          jlabel= new JLabel(""+currentLetter);
+         type.run();
+		
+		
+		
+		
+		
+		
+        
+        
+        
+        
+        
+        
+        
+        
 	}
 
 }
--- src/_06_book_of_illusions/BookOfIllusions.java
+++ src/_06_book_of_illusions/BookOfIllusions.java
@@ -11,6 +11,7 @@ import java.net.URL;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
  
 /** We’re going to make a slideshow of cool optical illusions. When the user clicks on an illusion, a new one will be loaded. **/
@@ -18,9 +19,12 @@ import javax.swing.JLabel;
 public class BookOfIllusions extends MouseAdapter {
 
 	// 1. Make a JFrame variable and initialize it using "new JFrame()"
-
+         JFrame jframe= new JFrame("BookOfIllusions");
+	
 	public void run() {
 		// 2. make the frame visible
+		jframe.setSize(800, 800);
+		jframe.setVisible(true);
 		// 3. set the size of the frame
 		// 4. find 2 images and save them to your project’s default package
 		// 5. make a variable to hold the location of your image. e.g. "illusion.jpg"
